9131fea15a04c2c6bfcd535fb6e4704d6c487c0a
Salman <salmanhammed77@gmail.com>
2024-05-22 19:34:58 +0530

    update migration script add migration list and push

--- web/script/new-migration.js
+++ web/script/new-migration.js
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process';
+import { exec, spawn } from 'child_process';
 import * as readline from 'readline';
 
 const dryRunProcess = spawn('supabase', ['db', 'diff']);
@@ -34,8 +34,17 @@ function executeMigration(migrationName, rl) {
 		console.error(data.toString());
 	});
 
-	supabaseProcess.on('close', (code) => {
-		console.log(`Supabase process exited with code ${code}`);
+	supabaseProcess.on('close', () => {
+		console.log("Migration successfully executed");
+
+		exec('supabase migration list', (err, stdout) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(stdout);
+		});
+		console.log("Run 'supabase db push' to sync to remote db")
 		rl.close();
 	});
 }
