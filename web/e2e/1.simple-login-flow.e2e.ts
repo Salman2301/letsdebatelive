c6bdb624a9ddface24c8bd948fdc3cfe9de48dc1
Salman <salmanhammed77@gmail.com>
2024-05-27 18:44:15 +0530

    init e2e

---
+++ web/e2e/1.simple-login-flow.e2e.ts
@@ -0,0 +1,35 @@
+import { expect, test } from '@playwright/test';
+
+test('index page should have logo', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('link', { name: 'LETSDEBATE LIVE' })).toBeVisible();
+});
+
+
+test('anon control-room should be redirect back home', async ({ page }) => {
+	await page.goto('/profile/user3/control-room');
+
+	await expect(page.getByRole('heading', { name: 'Backstage' })).toBeHidden();
+});
+
+test('page login and control room should be visible', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('link', { name: 'LETSDEBATE LIVE' })).toBeVisible();
+
+	await page.getByRole("button").click();
+	
+	await page.getByRole('button', { name: 'Login' }).click();
+	// await page.waitForTimeout(300);
+	await page.locator('input[name="email"]').fill("TeamUser3-C@example.com");
+	await page.locator('input[name="password"]').fill("TeamUser3-C");
+	await page.locator('form').getByRole('button', { name: 'Login' }).click();
+	await page.waitForTimeout(300);
+
+	await page.getByRole("button").click();
+	
+	await expect(page.getByRole('button', { name: 'Control Room' })).toBeVisible();
+	await page.getByRole('button', { name: 'Control Room' }).click();
+	await page.waitForLoadState("domcontentloaded");
+	await page.waitForTimeout(300);
+	await expect(page.getByRole('heading', { name: 'Backstage' })).toBeVisible();
+});
--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -3,10 +3,13 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		reuseExistingServer: true,
 	},
-	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testDir: 'e2e',
+	testMatch: /(.*).e2e.ts/,
+	outputDir: "e2e-results",
+	snapshotDir: "e2e-snapshot",
 };
 
 export default config;
